e9f968e803785d5279ceeab3222d6fe90b013f3c
sndsabin <sndsabin@gmail.com>
2019-12-02 17:58:05 +0545

    fixes #1

--- ObjectsWithCsvBundle.php
+++ ObjectsWithCsvBundle.php
@@ -14,4 +14,20 @@ class ObjectsWithCsvBundle extends AbstractPimcoreBundle
             '/bundles/objectswithcsv/js/pimcore/objects/classes/tags/objectsWithCsv.js'
         ];
     }
+
+    /**
+     * @return string
+     */
+    public function getDescription()
+    {
+        return 'Facilitates user to upload csv file for bulk attaching to objects datatype using csv upload functionality.';
+    }
+
+    /**
+     * @return string
+     */
+    public function getVersion()
+    {
+        return '1.0.1';
+    }
 }
